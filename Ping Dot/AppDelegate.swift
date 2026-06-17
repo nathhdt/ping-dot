d15e678cf47aa1f7ce7984dfb82133aa481a9eee
nathhdt <nathhdt@pm.me>
2026-06-17 08:15:37 +0200

    refactor(app): remove implicitly unwrapped optional

--- Ping Dot/AppDelegate.swift
+++ Ping Dot/AppDelegate.swift
@@ -2,13 +2,13 @@ import Cocoa
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
 
-    private var statusBarController: StatusBarController!
+    private var statusBarController: StatusBarController?
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         statusBarController = StatusBarController()
     }
 
     func applicationWillTerminate(_ notification: Notification) {
-        statusBarController.stop()
+        statusBarController?.stop()
     }
 }
